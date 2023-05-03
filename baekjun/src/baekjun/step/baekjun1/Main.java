a389242e37e6baffdfb35d1a4af8670c5dd2b415
kimtokki <kht167993@gmail.com>
2023-05-03 10:42:05 +0900

    백준

---
+++ baekjun/src/baekjun/step/baekjun1/Main.java
@@ -0,0 +1,11 @@
+package baekjun.step.baekjun1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+	}
+
+}
+
+
---
+++ baekjun/src/baekjun/step/baekjun2/Main1.java
@@ -0,0 +1,10 @@
+package baekjun.step.baekjun2;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
